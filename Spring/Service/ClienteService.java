ff8bf6d4e0994654dfedf731ec454748512fd31b
João Augusto <80292739+JoaoASE@users.noreply.github.com>
2024-03-05 08:29:27 -0300

    Update ClienteService.java

--- Spring/Service/ClienteService.java
+++ Spring/Service/ClienteService.java
@@ -1,5 +1,29 @@
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+public class ClienteService {
+    private ClienteRepository clienteRepository;
 
+    public ClienteService(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    public Cliente cadastrarCliente(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    public Cliente atualizarCliente(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    public void excluirCliente(Long id) {
+        clienteRepository.deleteById(id);
+    }
+
+    public Cliente buscarCliente(Long id) {
+        return clienteRepository.findById(id).orElse(null);
+    }
+
+    public List<Cliente> listarClientes() {
+        return clienteRepository.findAll();
+    }
 }
