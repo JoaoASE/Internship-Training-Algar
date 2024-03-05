8acaf963d1c1074165dea40c5a41591bda798ce0
João Augusto <80292739+JoaoASE@users.noreply.github.com>
2024-03-05 08:18:47 -0300

    Create Pagamento.java

---
+++ Spring/Pagamento.java
@@ -0,0 +1,27 @@
+import java.math.BigDecimal;
+
+public class Pagamento {
+    private Long id;
+    private BigDecimal valor;
+
+    public Pagamento(Long id, BigDecimal valor) {
+        this.id = id;
+        this.valor = valor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+}
