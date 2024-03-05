af6e355c5f56a38861c39739f2c2ab717a4ea983
João Augusto <80292739+JoaoASE@users.noreply.github.com>
2024-03-05 08:29:41 -0300

    Update PagamentoService.java

--- Spring/Service/PagamentoService.java
+++ Spring/Service/PagamentoService.java
@@ -1,5 +1,29 @@
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
-public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
-    
+public class PagamentoService {
+    private PagamentoRepository pagamentoRepository;
+
+    public PagamentoService(PagamentoRepository pagamentoRepository) {
+        this.pagamentoRepository = pagamentoRepository;
+    }
+
+    public Pagamento cadastrarPagamento(Pagamento pagamento) {
+        return pagamentoRepository.save(pagamento);
+    }
+
+    public Pagamento atualizarPagamento(Pagamento pagamento) {
+        return pagamentoRepository.save(pagamento);
+    }
+
+    public void excluirPagamento(Long id) {
+        pagamentoRepository.deleteById(id);
+    }
+
+    public Pagamento buscarPagamento(Long id) {
+        return pagamentoRepository.findById(id).orElse(null);
+    }
+
+    public List<Pagamento> listarPagamentos() {
+        return pagamentoRepository.findAll();
+    }
 }
