a55ef92e3159913ac04539e4366ebdd1aac836a1
João Augusto <80292739+JoaoASE@users.noreply.github.com>
2024-03-05 08:29:12 -0300

    Update RecargaRepository.java

--- Spring/Repository/RecargaRepository.java
+++ Spring/Repository/RecargaRepository.java
@@ -1,1 +1,7 @@
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+public interface RecargaRepository extends JpaRepository<Recarga, Long> {
+    List<Recarga> findByClienteId(Long clienteId);
+    
+}
